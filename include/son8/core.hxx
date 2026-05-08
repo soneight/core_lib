9f53b315b3bc75e0cacd1e6449689f9746d2780a
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-05-08 17:27:56 +0300

    add core pour arrays alias types

--- include/son8/core.hxx
+++ include/son8/core.hxx
@@ -1,9 +1,17 @@
 #ifndef SON8_CORE_HXX
 #define SON8_CORE_HXX
 
-namespace son8 {
-
-} // namespace son8
+namespace son8::core {
+    // POUR ARRays
+    template< typename Type > using Ptr = Type *;
+    template< typename Type > using Out = Type &;
+    template< typename Type > using Uni = Type &&;
+    template< typename Type > using Ref = Type const &;
+    template< typename Type, unsigned Size > using PtrArr = Ptr< Type[Size] >;
+    template< typename Type, unsigned Size > using OutArr = Out< Type[Size] >;
+    template< typename Type, unsigned Size > using UniArr = Uni< Type[Size] >;
+    template< typename Type, unsigned Size > using RefArr = Ref< Type[Size] >;
+} // namespace son8::core
 
 #endif//SON8_CORE_HXX
 
