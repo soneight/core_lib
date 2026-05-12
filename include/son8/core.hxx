9334de33376b8a1d253846993b2123449d25d045
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-05-12 19:56:19 +0300

    add c base and cxx core type aliases

--- include/son8/core.hxx
+++ include/son8/core.hxx
@@ -1,16 +1,36 @@
 #ifndef SON8_CORE_HXX
 #define SON8_CORE_HXX
 
+#include <son8/c/base.hxx>
+#include <son8/cxx/core.hxx>
+
 namespace son8::core {
     // POUR ARRays
     template< typename Type > using Ptr = Type *;
     template< typename Type > using Out = Type &;
     template< typename Type > using Uni = Type &&;
     template< typename Type > using Ref = Type const &;
-    template< typename Type, unsigned Size > using PtrArr = Ptr< Type[Size] >;
-    template< typename Type, unsigned Size > using OutArr = Out< Type[Size] >;
-    template< typename Type, unsigned Size > using UniArr = Uni< Type[Size] >;
-    template< typename Type, unsigned Size > using RefArr = Ref< Type[Size] >;
+    template< typename Type, unsigned Size > using PtrArr = Ptr< Type[Size] >; // Type (*)[Size]
+    template< typename Type, unsigned Size > using OutArr = Out< Type[Size] >; // Type (&)[Size]
+    template< typename Type, unsigned Size > using UniArr = Uni< Type[Size] >; // Type (&&)[Size]
+    template< typename Type, unsigned Size > using RefArr = Ref< Type[Size] >; // Type const (&)[Size]
+    // c base type aliases
+    using Char = unsigned char;
+    using Diff = c::ptrdiff_t;
+    using Int0 = c::int8_t;  // 2^0 eq 1 byte signed
+    using Int1 = c::int16_t; // 2^1 eq 2 byte signed
+    using Int2 = c::int32_t; // 2^2 eq 4 byte signed
+    using Int3 = c::int64_t; // 2^3 eq 8 byte signed
+    using Size = c::size_t;
+    using Unt0 = c::uint8_t;  // 2^0 eq 1 byte unsigned
+    using Unt1 = c::uint16_t; // 2^1 eq 2 byte unsigned
+    using Unt2 = c::uint32_t; // 2^2 eq 4 byte unsigned
+    using Unt3 = c::uint64_t; // 2^3 eq 8 byte unsigned
+    // cxx core type aliases
+    template< typename Type, unsigned Size >
+    using Array = cxx::array< Type, Size >;
+    template< typename Type >
+    using Init = cxx::initializer_list< Type >;
 } // namespace son8::core
 
 #endif//SON8_CORE_HXX
