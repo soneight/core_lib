c3984dff55c0234402cc973e15364eca500f11a7
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-07-14 13:01:02 +0300

    progress alias core and data

---
+++ include/son8/core/alias.hxx
@@ -0,0 +1,49 @@
+#ifndef SON8_CORE_ALIAS_HXX
+#define SON8_CORE_ALIAS_HXX
+
+#include <son8/core/alias/data.hxx>
+
+#include <son8/c/base.hxx>
+#include <son8/cxx/core.hxx>
+
+namespace son8::core {
+    // `POUR ARRays`
+    template< typename Type >
+    using Ptr = Type *; // pointer to Type
+    template< typename Type >
+    using Out = Type &; // `lvalue reference` to Type
+    template< typename Type >
+    using Uni = Type &&; // `rvalue reference` to Type
+    template< typename Type >
+    using Ref = Type const &; // `lvalue reference` to constant Type
+    template< typename Type, unsigned Size >
+    using PtrArr = Ptr< Type[Size] >; // Type `(*)[Size]`: `pointer` to C-array
+    template< typename Type, unsigned Size >
+    using OutArr = Out< Type[Size] >; // Type `(&)[Size]`: `lvalue reference` to C-array
+    template< typename Type, unsigned Size >
+    using UniArr = Uni< Type[Size] >; // Type `(&&)[Size]`: `rvalue reference` to C-array
+    template< typename Type, unsigned Size >
+    using RefArr = Ref< Type[Size] >; // Type `const (&)[Size]`: `lvalue reference` to constant C-array
+    // `c` base type aliases
+    using Char = char;
+    using Diff = c::ptrdiff_t;
+    using Int0 = c::int8_t;  // `2^0 eq 1` byte signed
+    using Int1 = c::int16_t; // `2^1 eq 2` byte signed
+    using Int2 = c::int32_t; // `2^2 eq 4` byte signed
+    using Int3 = c::int64_t; // `2^3 eq 8` byte signed
+    using Size = c::size_t;
+    using Unt0 = c::uint8_t;  // `2^0 eq 1` byte unsigned
+    using Unt1 = c::uint16_t; // `2^1 eq 2` byte unsigned
+    using Unt2 = c::uint32_t; // `2^2 eq 4` byte unsigned
+    using Unt3 = c::uint64_t; // `2^3 eq 8` byte unsigned
+    // `cxx` core type aliases
+    template< typename Type, unsigned size >
+    using Array [[deprecated("renamed, subject to be removed on release - use Flat instead")]] = cxx::array< Type, size >;
+    template< typename Type, unsigned size >
+    using Flat = cxx::array< Type, size >;
+    template< typename Type >
+    using Init = cxx::initializer_list< Type >;
+
+} // namespace `son8::core`
+
+#endif // header
---
+++ include/son8/core/alias/data.hxx
@@ -0,0 +1,62 @@
+#ifndef SON8_CORE_ALIAS_DATA_HXX
+#define SON8_CORE_ALIAS_DATA_HXX
+
+#include <son8/cxx/data.hxx>
+
+namespace son8::core {
+    // only uses default values for templated params
+    // \ for fancy staff use `cxx` or `std` directly
+    // -- standard allocators
+    template < typename Type >
+    using Deck = cxx::deque< Type >;
+    template < typename Type >
+    using Grow = cxx::vector< Type >;
+    template< typename Key_Type, typename Value_Type >
+    using Hash = cxx::unordered_map< Key_Type, Value_Type >;
+    template< typename Type >
+    using Keys = cxx::unordered_set< Type >;
+    template< typename Type >
+    using List = cxx::list< Type >;
+    template< typename Type >
+    using Tail = cxx::forward_list< Type >;
+    template< typename Type >
+    using Tide = cxx::set< Type >;
+    template< typename Key_Type, typename Value_Type >
+    using Tree = cxx::map< Key_Type, Value_Type >;
+    // -- multiple
+    template< typename Key_Type, typename Value_Type >
+    using HashMany = cxx::unordered_multimap< Key_Type, Value_Type >;
+    template< typename Type >
+    using KeysMany = cxx::unordered_multiset< Type >;
+    template< typename Type >
+    using TideMany = cxx::multiset< Type >;
+    template< typename Key_Type, typename Value_Type >
+    using TreeMany = cxx::multimap< Key_Type, Value_Type >;
+    // -- adaptors
+    template< typename Type >
+    using Order = cxx::priority_queue< Type >;
+    template< typename Type >
+    using Queue = cxx::queue< Type >;
+    template< typename Type >
+    using Stack = cxx::stack< Type >;
+    // -- polymorphic allocators
+    template < typename Type >
+    using PolyDeck = cxx::pmr::deque< Type >;
+    template< typename Type >
+    using PolyGrow = cxx::pmr::vector< Type >;
+    template< typename Key_Type, typename Value_Type >
+    using PolyHash = cxx::pmr::unordered_map< Key_Type, Value_Type >;
+    template< typename Type >
+    using PolyKeys = cxx::pmr::unordered_set< Type >;
+    template< typename Type >
+    using PolyList = cxx::pmr::list< Type >;
+    template< typename Type >
+    using PolyTail = cxx::pmr::forward_list< Type >;
+    template< typename Type >
+    using PolyTide = cxx::pmr::set< Type >;
+    template< typename Key_Type, typename Value_Type >
+    using PolyTree = cxx::pmr::map< Key_Type, Value_Type >;
+
+} // namespace
+
+#endif // header
